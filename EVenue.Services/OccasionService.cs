1b9e4d2e94cace1286cf013f8f7b310f7de446b3
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-06-08 15:41:58 -0400

    add OccasionService with CreateOccasion method

---
+++ EVenue.Services/OccasionService.cs
@@ -0,0 +1,43 @@
+﻿using EVenue.Data;
+using EVenue.Models.OccasionModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVenue.Services
+{
+    public class OccasionService
+    {
+        //declare userId field
+        private readonly Guid _userId;
+
+        //constructor to set userId field
+        public OccasionService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        //CRUD methods
+        //CREATE/POST
+        public bool CreateOccasion(OccasionCreate model)
+        {
+            var entity = new Occasion
+            {
+                OwnerId = _userId,
+                OccasionName = model.OccasionName,
+                StartTime = model.StartTime,
+                EndTime = model.EndTime,
+                VenueProfileId = model.VenueProfileId,
+                CustomerId = model.CustomerId
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Occasions.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
