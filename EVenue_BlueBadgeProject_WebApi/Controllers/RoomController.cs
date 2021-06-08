4aa6fc72a224c6cbba9a00655a50742a26b806d2
Alexandro Cazares <CazaresCode@gmail.com>
2021-06-08 16:10:18 -0400

    Create GET and POST methods in Room Controller.

--- EVenue_BlueBadgeProject_WebApi/Controllers/RoomController.cs
+++ EVenue_BlueBadgeProject_WebApi/Controllers/RoomController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EVenue.Models.RoomModels;
+using EVenue.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,5 +13,26 @@ namespace EVenue_BlueBadgeProject_WebApi.Controllers
     [Authorize]
     public class RoomController : ApiController
     {
+
+        public IHttpActionResult Post(RoomCreate room)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateRoomService();
+
+            if (!service.CreateRoom(room))
+                return InternalServerError();
+
+            return Ok(room);
+        }
+
+        private RoomService CreateRoomService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var roomService = new RoomService(userId);
+            return roomService;
+        }
+
     }
 }
