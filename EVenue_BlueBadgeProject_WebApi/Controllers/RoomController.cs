9edf035369caec3b14bb232d262ecef1a2991b36
Alexandro Cazares <CazaresCode@gmail.com>
2021-06-08 14:20:08 -0400

    Create classes for Services, Models, and WebAPI.

---
+++ EVenue_BlueBadgeProject_WebApi/Controllers/RoomController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EVenue_BlueBadgeProject_WebApi.Controllers
+{
+    [Authorize]
+    public class RoomController : ApiController
+    {
+    }
+}
