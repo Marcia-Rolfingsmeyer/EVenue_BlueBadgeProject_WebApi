3817739d4ee7d09674b7ac2bbf581dc504b0fa7b
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-06-08 14:33:47 -0400

    add OccasionCreate model

---
+++ EVenue.Models/OccasionModels/OccasionCreate.cs
@@ -0,0 +1,25 @@
+﻿using EVenue.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVenue.Models.OccasionModels
+{
+    public class OccasionCreate
+    {
+        public string OccasionName { get; set; }
+
+        public OccasionType TypeOfOccasion { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public int VenueProfileId { get; set; }
+
+        public int CustomerId { get; set; }
+    }
+}
