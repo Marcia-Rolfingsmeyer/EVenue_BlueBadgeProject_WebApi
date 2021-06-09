6aa47f361e454c3019cf58b0b5f957aef0ecf898
Alexandro Cazares <CazaresCode@gmail.com>
2021-06-08 21:08:38 -0400

    Create vendor Models.

--- EVenue.Models/VendorModels/VendorListItem.cs
+++ EVenue.Models/VendorModels/VendorListItem.cs
@@ -5,23 +5,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EVenue.Data
+namespace EVenue.Models.VendorModels
 {
-    public class Vendor
+    public class VendorListItem
     {
-        [Key]
         public int VendorId { get; set; }
 
-        [Required]
-        public Guid OwnerId { get; set; }
-
-        [Required]
         [Display(Name = "Vendor's Name")]
         public string VendorName { get; set; }
 
         //public VendorType TypeOfVendor { get; set; }
 
-        [Required]
         [Display(Name = "Vendor's Fee")]
         public double VendorFee { get; set; }
     }
