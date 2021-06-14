c5897cc34a645460a35209ee63410820ede1d59f
Marcia Rolfingsmeyer <rolfingsmeyer@gmail.com>
2021-06-14 16:28:31 -0400

    joint table - customer occasion - added create method and folders - service/model/controller

---
+++ EVenue.Services/CustomerOccasionService.cs
@@ -0,0 +1,35 @@
+﻿using EVenue.Data;
+using EVenue.Data.JointTables;
+using EVenue.Models.CustomerOccasionModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVenue.Services
+{
+    public class CustomerOccasionService
+    {
+        private readonly Guid _ownerId;
+        public CustomerOccasionService(Guid ownerId)
+        {
+            _ownerId = ownerId;
+        }
+
+        public bool CreateCustomerOccasion(CustomerOccasionCreate model)
+        {
+            var entity =
+                new CustomerOccasion()
+                {
+                    CustomerId = model.CustomerId,
+                    OccasionId = model.OccasionId
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.CustomerOccasions.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
