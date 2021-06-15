896060d5722fdcfd00be2695b590b87b01453881
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-06-15 09:27:59 -0500

    debug CustomerOccasionModels folder project error

---
+++ EVenue.Models/CustomerOccasionDetail.cs
@@ -0,0 +1,18 @@
+﻿using EVenue.Models.CustomerModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVenue.Models.CustomerOccasionModels
+{
+    public class CustomerOccasionDetail
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public CustomerListItem Customer { get; set; }
+        public int OccasionId { get; set; }
+        //public OccasionListItem Occasion { get; set; }
+    }
+}
